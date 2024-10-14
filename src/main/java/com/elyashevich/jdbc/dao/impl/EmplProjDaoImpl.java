e5cf47f2d4a6748fd160d2f559997a736ddae592
Artem-Elyashevich <elyashevich.artem06@gmail.com>
2024-10-15 00:11:04 +0300

    Add: employee and employe_project dao layers

--- src/main/java/com/elyashevich/jdbc/dao/impl/EmplProjDaoImpl.java
+++ src/main/java/com/elyashevich/jdbc/dao/impl/EmplProjDaoImpl.java
@@ -3,31 +3,123 @@ package com.elyashevich.jdbc.dao.impl;
 import com.elyashevich.jdbc.dao.EmplProjDao;
 import com.elyashevich.jdbc.entity.EmplProj;
 
+import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class EmplProjDaoImpl implements EmplProjDao {
+
+    private final Connection connection;
+
+    private final static String SELECT_ALL_QUERY =
+            "SELECT EMPLOYEE_ID, PROJECT_ID FROM EMPL_PROJ";
+    private final static String SELECT_BY_ID_QUERY =
+            "SELECT EMPLOYEE_ID, PROJECT_ID FROM EMPL_PROJ WHERE ID=?";
+    private final static String INSERT_QUERY =
+            "INSERT INTO EMPL_PROJ (EMPLOYEE_ID, PROJECT_ID) VALUES (?, ?)";
+
+    private final static String UPDATE_QUERY = "UPDATE EMPL_PROJ SET EMPLOYEE_ID=?, PROJECT_ID=? WHERE EMPLOYEE_ID=? AND PROJECT_ID=?";
+    private final static String DELETE_QUERY = "DELETE FROM EMPL_PROJ WHERE ID=?";
+
+    public EmplProjDaoImpl(Connection connection) {
+        this.connection = connection;
+    }
+
     @Override
-    public List<EmplProj> findAll() {
+    public List<EmplProj> findAll() throws SQLException {
+        List<EmplProj> result = new ArrayList<>();
+
+        try (Statement statement = this.connection.createStatement()) {
+
+            ResultSet resultSet = statement.executeQuery(SELECT_ALL_QUERY);
+
+            while (resultSet.next()) {
+                EmplProj emplProj = new EmplProj();
+
+                emplProj.setEmployeeId(resultSet.getLong("EMPLOYEE_ID"));
+                emplProj.setProjectId(resultSet.getLong("PROJECT_ID"));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            if (this.connection != null) {
+                this.connection.close();
+            }
+        }
         return null;
     }
 
     @Override
-    public EmplProj findById(Long id) {
-        return null;
+    public EmplProj findById(Long id) throws SQLException {
+        EmplProj emplProj = new EmplProj();
+        try (PreparedStatement preparedStatement = this.connection.prepareStatement(SELECT_BY_ID_QUERY)) {
+            preparedStatement.setLong(1, id);
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            emplProj.setEmployeeId(resultSet.getLong("EMPLOYEE_ID"));
+            emplProj.setProjectId(resultSet.getLong("PROJECT_ID"));
+
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            if (this.connection != null) {
+                this.connection.close();
+            }
+        }
+        return emplProj;
     }
 
     @Override
-    public void create(EmplProj EmplProj) {
+    public void create(EmplProj emplProj) throws SQLException {
+        try (PreparedStatement preparedStatement = this.connection.prepareStatement(INSERT_QUERY)) {
+            preparedStatement.setLong(1, emplProj.getEmployeeId());
+            preparedStatement.setLong(2, emplProj.getProjectId());
 
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            if (this.connection != null) {
+                this.connection.close();
+            }
+        }
     }
 
     @Override
-    public void update(EmplProj EmplProj) {
+    public void update(EmplProj emplProj) throws SQLException {
+        try (PreparedStatement preparedStatement = this.connection.prepareStatement(UPDATE_QUERY)) {
+            preparedStatement.setLong(1, emplProj.getEmployeeId());
+            preparedStatement.setLong(2, emplProj.getProjectId());
+            preparedStatement.setLong(3, emplProj.getEmployeeId());
+            preparedStatement.setLong(4, emplProj.getProjectId());
 
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            if (this.connection != null) {
+                this.connection.close();
+            }
+        }
     }
 
     @Override
-    public void remove(EmplProj EmplProj) {
+    public void remove(EmplProj emplProj) throws SQLException {
+        try (PreparedStatement preparedStatement = this.connection.prepareStatement(DELETE_QUERY)) {
+            preparedStatement.setLong(1, emplProj.getEmployeeId());
+            preparedStatement.setLong(2, emplProj.getProjectId());
+            preparedStatement.setLong(3, emplProj.getEmployeeId());
+            preparedStatement.setLong(4, emplProj.getProjectId());
 
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            if (this.connection != null) {
+                this.connection.close();
+            }
+        }
     }
 }
--- src/main/java/com/elyashevich/jdbc/dao/impl/EmployeeDaoImpl.java
+++ src/main/java/com/elyashevich/jdbc/dao/impl/EmployeeDaoImpl.java
@@ -3,31 +3,136 @@ package com.elyashevich.jdbc.dao.impl;
 import com.elyashevich.jdbc.dao.EmployeeDao;
 import com.elyashevich.jdbc.entity.Employee;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 public class EmployeeDaoImpl implements EmployeeDao {
-    @Override
-    public List<Employee> findAll() {
-        return null;
+
+    private final Connection connection;
+
+    private final static String SELECT_ALL_QUERY =
+            "SELECT ID, FIRST_NAME, LAST_NAME, BIRTHDAY, ADDRESS_ID FROM EMPLOYEE";
+    private final static String SELECT_BY_ID_QUERY =
+            "SELECT ID, FIRST_NAME, LAST_NAME, BIRTHDAY, ADDRESS_ID FROM EMPLOYEE WHERE ID=?";
+    private final static String INSERT_QUERY =
+            "INSERT INTO EMPLOYEE (ID, FIRST_NAME, LAST_NAME, BIRTHDAY, ADDRESS_ID) VALUES(?, ?, ?, ?, ?)";
+    private final static String UPDATE_QUERY = "UPDATE EMPLOYEE SET FIRST_NAME=?, LAST_NAME=?, BIRTHDAY=?, ADDRESS_ID=? WHERE ID=?";
+    private final static String DELETE_QUERY = "DELETE FROM EMPLOYEE WHERE ID=?";
+
+    public EmployeeDaoImpl(Connection connection) {
+        this.connection = connection;
     }
 
     @Override
-    public Employee findById(Long id) {
-        return null;
+    public List<Employee> findAll() throws SQLException {
+        List<Employee> employees = new ArrayList<>();
+
+        try (Statement statement = this.connection.createStatement()) {
+
+            ResultSet resultSet = statement.executeQuery(SELECT_ALL_QUERY);
+
+            while (resultSet.next()) {
+                Employee employee = new Employee();
+
+                employee.setId(resultSet.getLong("ID"));
+                employee.setFirstname(resultSet.getString("FIRST_NAME"));
+                employee.setLastname(resultSet.getString("LAST_NAME"));
+                employee.setAddressId(resultSet.getLong("ADDRESS_ID"));
+                employee.setBirthDate(resultSet.getDate("BIRTHDATE"));
+
+                employees.add(employee);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            if (this.connection != null) {
+                this.connection.close();
+            }
+        }
+        return employees;
     }
 
     @Override
-    public void create(Employee Employee) {
+    public Employee findById(Long id) throws SQLException {
+        Employee employee = new Employee();
 
+        try (PreparedStatement preparedStatement = this.connection.prepareStatement(SELECT_BY_ID_QUERY)) {
+            preparedStatement.setLong(1, id);
+
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            employee.setId(resultSet.getLong("ID"));
+            employee.setFirstname(resultSet.getString("FIRST_NAME"));
+            employee.setLastname(resultSet.getString("LAST_NAME"));
+            employee.setBirthDate(resultSet.getDate("BIRTHDATE"));
+            employee.setAddressId(resultSet.getLong("ADDRESS_ID"));
+
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            if (this.connection != null) {
+                this.connection.close();
+            }
+        }
+        return employee;
     }
 
     @Override
-    public void update(Employee Employee) {
+    public void create(Employee employee) throws SQLException {
+        try (PreparedStatement preparedStatement = this.connection.prepareStatement(INSERT_QUERY)) {
+            preparedStatement.setLong(1, employee.getId());
+            preparedStatement.setString(2, employee.getFirstname());
+            preparedStatement.setString(3, employee.getLastname());
+            preparedStatement.setDate(4, employee.getBirthDate());
+            preparedStatement.setLong(5, employee.getAddressId());
 
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            if (this.connection != null) {
+                this.connection.close();
+            }
+        }
     }
 
     @Override
-    public void remove(Employee Employee) {
+    public void update(Employee employee) throws SQLException {
+        try (PreparedStatement preparedStatement = this.connection.prepareStatement(UPDATE_QUERY)) {
+            preparedStatement.setString(1, employee.getFirstname());
+            preparedStatement.setString(2, employee.getLastname());
+            preparedStatement.setDate(3, employee.getBirthDate());
+            preparedStatement.setLong(4, employee.getAddressId());
+            preparedStatement.setLong(5, employee.getId());
 
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            if (this.connection != null) {
+                this.connection.close();
+            }
+        }
+    }
+
+    @Override
+    public void remove(Employee employee) throws SQLException {
+        try (PreparedStatement preparedStatement = this.connection.prepareStatement(DELETE_QUERY)) {
+            preparedStatement.setLong(1, employee.getId());
+
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            if (this.connection != null) {
+                this.connection.close();
+            }
+        }
     }
 }
